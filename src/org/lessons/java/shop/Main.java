6665ec661d5b4cfe81e2cd2fcccc717dc247cca9
Peppe2496 <giuseppemusellajob@gmail.com>
2023-12-06 16:03:15 +0100

    ended without bonus

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,2 +1,34 @@
-package org.lessons.java.shop;public class Main {
+package org.lessons.java.shop;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many products ?");
+        int numberOfProducts = Integer.parseInt(scanner.nextLine());
+
+        Product[] listOfProducts = new Product[numberOfProducts];
+
+        for (int i = 0; i < listOfProducts.length ; i++) {
+            System.out.println("What's prduct's name ?");
+            String productName = scanner.nextLine();
+            System.out.println("Describe the product");
+            String productDescription = scanner.nextLine();
+            System.out.println("How much does the product cost");
+            double productPrice = Double.parseDouble(scanner.nextLine());
+            Product product = new Product(productName,productDescription,productPrice);
+            listOfProducts[i] = product;
+        }
+        System.out.println("Your products");
+        for (int i = 0; i < listOfProducts.length ; i++) {
+            System.out.println("Product:" + listOfProducts[i].getProductName()+" "
+                    + listOfProducts[i].getProductCode()+" "
+                    +"Description:"+" "
+                    +listOfProducts[i].getProductDescription()+" "
+                    +"Price"+listOfProducts[i].getProductPrice()+"$"+" "
+                    +"Price with IVA:"+ listOfProducts[i].getIva()+"$");
+        }
+      scanner.close();
+    }
 }
--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -1,2 +1,72 @@
-package org.lessons.java.shop;public class Product {
+package org.lessons.java.shop;
+
+import java.util.Random;
+
+public class Product {
+
+            //Attributi
+
+            private int productCode;
+            private String productName;
+            private String productDescription;
+            private double productPrice;
+            private double iva;
+
+            //Costruttori
+
+            public Product(String productName, String productDescription, double productPrice) {
+                this.productCode = productCode;
+                this.productName = productName;
+                this.productDescription = productDescription;
+                this.productPrice = productPrice;
+                this.iva = iva;
+            }
+            //getter and setter
+
+            public int getProductCode(){
+                int productCode = randomCode.nextInt(10000,99999);
+                return productCode;
+            }
+
+            public String getProductName(){
+                return productName;
+            }
+
+           public String getProductDescription() {
+              return productDescription;
+           }
+
+           public double getProductPrice() {
+               return productPrice;
+           }
+
+           public double getIva() {
+                iva = productPrice*1.22;
+               return iva;
+          }
+
+          public void setProductName(String  productName){
+                this.productName = productName;
+          }
+
+          public void setProductDescription(String productDescription){
+                this.productDescription = productDescription;
+          }
+
+          public void  setProductPrice(double productPrice){
+                this.productPrice = productPrice;
+          }
+
+          public void setIva(double iva){
+                this.iva = iva;
+          }
+
+          //Metodi
+
+         Random randomCode = new Random();
+
+            public int randomProductCode = randomCode.nextInt(10000,999999);
+
+            public double priceIva = productPrice * 1.22;
+
 }
